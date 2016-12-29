f1370e8f74f1c4d67c7ef1e32346bb84c96721ca
Alex Sun <a.sun@sap.com>
2016-12-29 16:35:13 +0800

    Add ThreadLocal implementation theory into ThreadLocalDemo

--- com.sun.example1/src/com/sun/example/java/concurrent/ThreadLocalDemo.java
+++ com.sun.example1/src/com/sun/example/java/concurrent/ThreadLocalDemo.java
@@ -1,23 +1,34 @@
 package com.sun.example.java.concurrent;
 
+/**
+ * ThreadLocal实现原理
+ * 1. 每个Thread都有一个本地变量threadLocals, 类型是ThreadLocal.ThreadLocalMap，用于存放和这个Thread相关的专属对象/值
+ * 2. 在主线程中申请了ThreadLocal类型的实例变量tl后，在子线程thread-n中引用ThreadLocal的实例变量tl进行set操作，在set方法中
+ *    会引用子线程thread-n对应的threadLocals(即ThreadLocalMap)，把主线程中的value作为key，需要设置的值作为value加入Map中。
+ * 3. 如果一个子线程引用多个tl类型的实例变量，则在该子线程的threadLocals中会有多个entry，分别以这些实例变量为key进行存储。
+ * 4. 因此主线程中的tl并不存储实际的变量，而只是变量设置的工具。
+ * @author I068353
+ *
+ */
+
 public class ThreadLocalDemo {
 	
-	private final ThreadLocal<Integer> value = new ThreadLocal<Integer>();
+	private final ThreadLocal<Integer> tl = new ThreadLocal<Integer>();
 	
 	public static void main(String[] args){
 		ThreadLocalDemo threadLocalDemo = new ThreadLocalDemo();
-		for(int i=0;i<100;i++){
+		for(int i=0;i<5;i++){
 			new Thread(threadLocalDemo.new MyThread()).start();
 		}
 	}
 	
 	public void calculate(Integer n){
-		value.set(0);
-		System.out.println("Thread: "+ Thread.currentThread() + " init value: " + value.get());
+		tl.set(0);
+		System.out.println("Thread: "+ Thread.currentThread() + " init value: " + tl.get());
 		for(int i=0;i<n;i++){
-			value.set( value.get() + i );
+			tl.set( tl.get() + i );
 		}
-		System.out.println("Thread: "+ Thread.currentThread() + " value: " + value.get());
+		System.out.println("Thread: "+ Thread.currentThread() + " value: " + tl.get());
 	}
 	
 	public class MyThread implements Runnable{
@@ -27,6 +38,11 @@ public class ThreadLocalDemo {
 			calculate(10000);			
 		}
 		
+		@Override
+		public String toString() {
+			return "MyThread [id=" + Thread.currentThread().getId() + ", name="
+					+ Thread.currentThread().getName() + "]";
+		}
 	}
 	
 }

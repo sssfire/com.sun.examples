8d68175a160ca748402be5ff300d18725d36422b
Alex Sun <a.sun@sap.com>
2017-01-03 19:42:34 +0800

    Generic Type Demo
    Change-Id: I98830c5672e2c899db8eba9daba7323cfc0a271e
    Signed-off-by: Alex Sun <a.sun@sap.com>

--- com.sun.example1/src/com/sun/example/java/generictype/CarShape.java
+++
@@ -1,9 +0,0 @@
-package com.sun.example.java.generictype;
-
-public class CarShape extends Shape{
-
-	public CarShape(String name) {
-		super(name);
-	}
-	
-}
--- com.sun.example1/src/com/sun/example/java/generictype/SuperCarShape.java
+++
@@ -1,9 +0,0 @@
-package com.sun.example.java.generictype;
-
-public class SuperCarShape extends CarShape{
-
-	public SuperCarShape(String name) {
-		super(name);
-	}
-	
-}

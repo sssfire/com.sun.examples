b2b44c176be845b3e65aedf18151cd91016433a4
Alex Sun <a.sun@sap.com>
2017-01-05 18:39:19 +0800

    Add SoftReferenceDemo
    Change-Id: Ia5e84694c240b794132847d3239d1f5ef1a8c2bf
    Signed-off-by: Alex Sun <a.sun@sap.com>

---
+++ com.sun.example1/src/com/sun/example/java/referencetype/SoftReferenceDemo.java
@@ -0,0 +1,147 @@
+package com.sun.example.java.referencetype;
+
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
+import java.util.Hashtable;
+import java.util.Random;
+
+public class SoftReferenceDemo {
+	
+	Hashtable<String, EmployeeReference> employeeCache = new Hashtable<String, EmployeeReference>();
+	ReferenceQueue<Employee> refQueue = new  ReferenceQueue<Employee>();
+	
+	//模拟数据库
+	Hashtable<String, Employee> emDatabase = new Hashtable<String, Employee>();
+	final static int employNum = 100000000;
+	
+	public static void main(String[] args) {
+		SoftReferenceDemo refDemo = new SoftReferenceDemo();
+		
+		for(int i=0;i<employNum;i++){
+			Random rand = new Random();
+			String key = (rand.nextInt(employNum) + 1) + "";
+			Employee em = refDemo.get(key);
+			//System.out.println(em);
+//			if( i%(employNum/10) == 0 ){
+//				System.out.println("System GC...");
+//				System.gc();
+//			}
+		}
+	}
+	
+	public SoftReferenceDemo() {
+		initalEmployeeData(employNum);
+	}
+	
+	public void put(Employee em){
+		EmployeeReference empRef = new EmployeeReference(em, refQueue);
+		employeeCache.put(em.getId(), empRef);
+		//System.out.println("Put ID: " + em.getId());
+	}
+	
+	public Employee get(String employeeID){
+		
+		EmployeeReference empRef = employeeCache.get(employeeID);
+		if(empRef!=null){ //曾经被缓存过
+			Employee em = empRef.get();
+			if(em!=null){ //employee未被回收
+				return em;
+			}else{
+				//清除无用缓存
+				System.out.println("准备清除缓存");
+				cleanCache();
+			}
+		}
+		
+		//未被缓存过，或缓存过但employee已被回收,重新缓存
+		Employee em = getEmployeeFromDatabase(employeeID);
+		put(em);
+		
+		return em;
+	}
+	
+	
+	//模拟数据库操作
+	private Employee getEmployeeFromDatabase(String employeeID){
+		return emDatabase.get(employeeID);
+	}
+	
+	private void cleanCache(){
+		EmployeeReference empRef = null;
+		
+		while((empRef=(EmployeeReference)refQueue.poll())!=null){
+			employeeCache.remove(empRef.getKey());
+			System.out.println("清理缓存：" + empRef.getKey());
+		}
+	}
+	
+	//创建模拟数据
+	private void initalEmployeeData(int number){
+		Random random = new Random();
+		for(int i=1;i<number+1;i++){
+			int age = random.nextInt(22) + 30; 
+			Employee em = new Employee(""+i, "name-"+i, age);
+			emDatabase.put(em.getId(), em);
+		}
+	}
+
+	class EmployeeReference extends WeakReference<Employee>{
+
+		private String key;
+		
+		public EmployeeReference(Employee em, ReferenceQueue<Employee> q) {
+			super(em, q);
+			this.key = em.getId();
+		}			
+		
+		public String getKey(){
+			return key;
+		}
+		
+	}	
+	
+	class Employee{
+		String id;
+		String name;
+		int age;
+		
+		public Employee(String id, String name, int age) {
+			super();
+			this.id = id;
+			this.name = name;
+			this.age = age;
+		}
+		
+		public String getId() {
+			return id;
+		}
+		
+		public void setId(String id) {
+			this.id = id;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		public int getAge() {
+			return age;
+		}
+		
+		public void setAge(int age) {
+			this.age = age;
+		}
+
+		@Override
+		public String toString() {
+			return "Employee [id=" + id + ", name=" + name + ", age=" + age + "]";
+		}
+		
+		
+	}
+}
